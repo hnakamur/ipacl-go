283881a19113ae7dd5da6c43b2a3642f88e031b5
Hiroaki Nakamura <hnakamur@gmail.com>
2023-11-05 11:03:02 +0900

    Update TestBinarySearch_String

--- binary_search_test.go
+++ binary_search_test.go
@@ -93,184 +93,23 @@ func TestBinarySearch_String(t *testing.T) {
 	}{
 		{
 			rules: "deny 192.0.2.0/28\ndeny 192.0.2.16/29",
-			want:  "BinarySearch{v4:[192.0.2.0-192.0.2.23], v6:[], action:deny}",
+			want:  "BinarySearch{v4:[!192.0.2.0-192.0.2.23], v6:[]}",
 		},
-		// {
-		// 	rules: "deny 192.0.2.0/28\ndeny 192.0.2.16/29\nallow 192.0.2.16/28\ndeny 192.0.2.24/29",
-		// 	want:  "BinarySearch{v4:[192.0.2.0-192.0.2.23], v6:[], action:deny}",
-		// },
-		// {
-		// 	rules: "deny 192.0.0.0/0\nallow 192.168.1.0/01\ndeny all",
-		// 	want:  "BinarySearch{v4Rules: [], v6Rules: [], action: allow}",
-		// },
-		// {
-		// 	rules: "deny 0.0.0.7\ndeny 0.0.0.1\ndeny 0.0.0.0",
-		// 	want:  "BinarySearch{v4Rules: [{start: 0.0.0.0, end: 0.0.0.1}, {start: 0.0.0.7, end: 0.0.0.7}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: "deny ::7\ndeny ::1\ndeny ::",
-		// 	want:  "BinarySearch{v4Rules: [], v6Rules: [{start: ::, end: ::1}, {start: ::7, end: ::7}], action: deny}",
-		// },
-		// {
-		// 	rules: "allow 0.0.0.7\ndeny 0.0.0.1\ndeny 0.0.0.0",
-		// 	want:  "BinarySearch{v4Rules: [{start: 0.0.0.0, end: 0.0.0.1}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: "allow ::7\ndeny ::1\ndeny ::",
-		// 	want:  "BinarySearch{v4Rules: [], v6Rules: [{start: ::, end: ::1}], action: deny}",
-		// },
-		// {
-		// 	rules: "allow 0.0.0.2\ndeny 0.0.0.1\ndeny 0.0.0.0",
-		// 	want:  "BinarySearch{v4Rules: [{start: 0.0.0.0, end: 0.0.0.1}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: "allow ::2\ndeny ::1\ndeny ::",
-		// 	want:  "BinarySearch{v4Rules: [], v6Rules: [{start: ::, end: ::1}], action: deny}",
-		// },
-		// {
-		// 	rules: "deny 192.0.0.1\nallow 0.0.0.1\nallow 192.0.0.0/7\nallow 0.0.0.0",
-		// 	want:  "BinarySearch{v4Rules: [{start: 192.0.0.1, end: 192.0.0.1}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 	deny  192.168.1.1
-		// 	allow 192.0.0.0/0
-		// 	deny  all
-		// `,
-		// 	want: "BinarySearch{v4Rules: [{start: 0.0.0.0, end: 192.168.1.0}, {start: 192.168.1.2, end: 255.255.255.255}], v6Rules: [], action: allow}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.2
-		// 		deny 192.0.2.1
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.1, end: 192.0.2.2}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.1
-		// 		deny 192.0.2.2
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.1, end: 192.0.2.2}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.1
-		// 		deny 192.0.2.2
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.1, end: 192.0.2.2}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.1
-		// 		deny 192.0.2.2
-		// 		deny 192.0.2.2/31
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.1, end: 192.0.2.3}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.0/30
-		// 		deny 192.0.2.2/31
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.0, end: 192.0.2.3}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.0/29
-		// 		deny 192.0.2.2/31
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.0, end: 192.0.2.7}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.1
-		// 		deny 192.0.2.3
-		// 		deny 192.0.2.5
-		// 		deny 192.0.2.0/29
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.0, end: 192.0.2.7}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.1
-		// 		deny 192.0.2.3
-		// 		deny 192.0.2.5
-		// 		allow 192.0.2.7
-		// 		deny 192.0.2.0/29
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.0, end: 192.0.2.6}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.1
-		// 		deny 192.0.2.3
-		// 		deny 192.0.2.5
-		// 		allow 192.0.2.7
-		// 		deny 192.0.2.0/28
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.0, end: 192.0.2.6}, {start: 192.0.2.8, end: 192.0.2.15}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.1
-		// 		deny 192.0.2.3
-		// 		deny 192.0.2.5
-		// 		allow 192.0.2.7
-		// 		deny 192.0.2.15
-		// 		deny 192.0.2.0/28
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.0, end: 192.0.2.6}, {start: 192.0.2.8, end: 192.0.2.15}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.1
-		// 		deny 192.0.2.3
-		// 		deny 192.0.2.5
-		// 		allow 192.0.2.7
-		// 		deny 192.0.2.16
-		// 		deny 192.0.2.0/28
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.0, end: 192.0.2.6}, {start: 192.0.2.8, end: 192.0.2.16}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.1
-		// 		deny 192.0.2.3
-		// 		deny 192.0.2.5
-		// 		allow 192.0.2.7
-		// 		allow 192.0.2.13
-		// 		allow 192.0.2.14
-		// 		deny 192.0.2.16
-		// 		deny 192.0.2.0/28
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.0, end: 192.0.2.6}, {start: 192.0.2.8, end: 192.0.2.12}, {start: 192.0.2.15, end: 192.0.2.16}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.1
-		// 		deny 192.0.2.3
-		// 		deny 192.0.2.5
-		// 		allow 192.0.2.7
-		// 		deny 192.0.2.16/28
-		// 		deny 192.0.2.0/28
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.0, end: 192.0.2.6}, {start: 192.0.2.8, end: 192.0.2.31}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.8/30
-		// 		deny 192.0.2.4/30
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.4, end: 192.0.2.11}], v6Rules: [], action: deny}",
-		// },
-		// {
-		// 	rules: `
-		// 		deny 192.0.2.8/30
-		// 		deny 192.0.2.6/31
-		// 	`,
-		// 	want: "BinarySearch{v4Rules: [{start: 192.0.2.6, end: 192.0.2.11}], v6Rules: [], action: deny}",
-		// },
+		{
+			rules: "deny 192.0.2.0/28\ndeny 192.0.2.24/29",
+			want:  "BinarySearch{v4:[!192.0.2.0-192.0.2.15, !192.0.2.24-192.0.2.31], v6:[]}",
+		},
+		{
+			rules: `
+				deny  192.168.1.1
+				allow 192.168.1.0/24
+				allow 10.1.1.0/16
+				allow 2001:0db8::/32
+				deny  all
+			`,
+			want: "BinarySearch{v4:[10.1.0.0-10.1.255.255, 192.168.1.0, 192.168.1.2-192.168.1.255], " +
+				"v6:[2001:db8::-2001:db8:ffff:ffff:ffff:ffff:ffff:ffff]}",
+		},
 	}
 	for i, tc := range testCases {
 		rules, defAct, err := ParseRuleLines(strings.NewReader(tc.rules))
@@ -279,7 +118,41 @@ func TestBinarySearch_String(t *testing.T) {
 		}
 		s := NewBinarySearch(rules, defAct)
 		if got := s.String(); got != tc.want {
-			t.Errorf("result mismatch, i=%d,\n got=%s\nwant=%s", i, got, tc.want)
+			t.Errorf("result mismatch, i=%d, rules=%s\n got=%s\nwant=%s", i, tc.rules, got, tc.want)
 		}
 	}
+}
+
+func FuzzBinarySearch_Lookup(f *testing.F) {
+	f.Add(`
+		deny  192.168.1.1
+		allow 192.168.1.0/24
+		allow 10.1.1.0/16
+		allow 2001:0db8::/32
+		deny  all
+		`, "192.168.1.1")
+	f.Add(`
+		deny  192.168.1.1
+		allow 192.168.1.0/24
+		allow 10.1.1.0/16
+		allow 2001:0db8::/32
+		deny  all
+		`, "2001:0db8::")
+	f.Fuzz(func(t *testing.T, s, input string) {
+		rules, defAct, err := ParseRuleLines(strings.NewReader(s))
+		if err != nil {
+			t.Skip()
+		}
+		target, err := netip.ParseAddr(input)
+		if err != nil || strings.Contains(target.String(), "%") {
+			t.Skip()
+		}
+		bs := NewBinarySearch(rules, defAct)
+		ls := newLinearSearch(rules, defAct)
+		got := bs.Lookup(target)
+		want := ls.Lookup(target)
+		if got != want {
+			t.Errorf("result mismatch, got=%s, want=%s", got, want)
+		}
+	})
 }
